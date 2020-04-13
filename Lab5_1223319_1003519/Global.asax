b5838ca5460913406ac08536c615f05999ca4622
Estuardoxd <Estuardovilleda2001@gmail.com>
2020-04-13 16:30:45 -0600

    creacion del proyecto

---
+++ Lab5_1223319_1003519/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab5_1223319_1003519.MvcApplication" Language="C#" %>
---
+++ Lab5_1223319_1003519/Models/Tareas.cs
@@ -0,0 +1,17 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab5_1223319_1003519.Models
+{
+    public class Tareas
+    {
+        public string Titulo { get; set; }
+        public string DescripcionTarea { get; set; }
+        public string Proyecto { get; set; }
+        public int Prioridad { get; set; }
+        public DateTime FechaEntrega { get; set; }
+    }
+    
+}
